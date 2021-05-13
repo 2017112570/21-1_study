5b03891df9e698f1b47c71496280bdc56531fb47
2017112570 <okh9712@gmail.com>
2021-05-13 19:21:09 +0900

    0513 study

--- 그리디/숫자 카드 게임.py
+++ 그리디/숫자 카드 게임.py
@@ -5,6 +5,7 @@ for i in range(n):
     data = list(map(int,input().split()))
     min_value = min(data)
     result = max(result,min_value)
+
 print(result)
 
 
@@ -13,8 +14,9 @@ n,m = map(int,input().split())
 result = 0
 for i in range(n):
     data = list(map(int,input().split()))
-    min_value=10001
+    min_value = 10001
     for  a in data:
         min_value = min(min_value,a)
     result = max(result,min_value)
+
 print(result)
