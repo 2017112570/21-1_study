7a9f71ffcbf0da87f101bc997c030b9c7a3a8ecc
2017112570 <okh9712@gmail.com>
2021-05-20 16:25:35 +0900

    second study

--- 구현(완전탐색)/상하좌우.py
+++ 구현(완전탐색)/상하좌우.py
@@ -1,5 +1,5 @@
 n = int(input())
-routes=input().split()
+routes=input().lower().split()
 x,y = 1,1
 for i in routes:
     if i == 'r':
@@ -25,20 +25,20 @@ for i in routes:
 print(x,y)
 
 ''' sol '''
-n=int(input())
-x,y = 1,1
-plans = input().split()
-# l,R,U,D에 따른 이동 방향
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
-moves_types=['L','R','U','D']
+# n=int(input())
+# x,y = 1,1
+# plans = input().split()
+# # l,R,U,D에 따른 이동 방향
+# dx=[0,0,-1,1]
+# dy=[-1,1,0,0]
+# move_types=['L','R','U','D']
 
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-        if nx < 1 or ny < 1 or nx > n or ny > n:
-            continue
-        x,y = nx,ny
-print(x,y)
+# for plan in plans:
+#     for i in range(len(move_types)):
+#         if plan == move_types[i]:
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#         if nx < 1 or ny < 1 or nx > n or ny > n:
+#             continue
+#         x,y = nx,ny
+# print(x,y)
--- 구현(완전탐색)/시각.py
+++ 구현(완전탐색)/시각.py
@@ -1,8 +1,11 @@
 n=int(input())
 count = 0
+
 for i in range(n+1):
     for j in range(60):
         for k in range(60):
+            # i,j,k 중 3이 있는 경우의 수
             if '3' in str(i)+str(j)+str(k):
                 count += 1
+
 print(count)
