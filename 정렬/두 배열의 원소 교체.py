6c0c730a3d172f45a09958b4f33cbb78da58dc7d
2017112570 <okh9712@gmail.com>
2021-06-17 11:58:29 +0900

    new

--- 정렬/두 배열의 원소 교체.py
+++ 정렬/두 배열의 원소 교체.py
@@ -2,12 +2,16 @@ n,k = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
 
+# 오름차순 정렬
 a.sort()
+# 내림차순 정렬
 b.sort(reverse=True)
 
 for i in range(k):
+    # A의 원소가 B보다 작을 때 바꿈
     if a[i] < b[i]:
-        a[i],b[i] = b[i],a[i]
-    else:
+        a[i], b[i] = b[i], a[i]
+    else: 
         break
+
 print(sum(a))
